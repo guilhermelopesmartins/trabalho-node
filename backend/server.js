eaa61b31fa63b7af91f387d1ac08b26aa7e7119c
Guilherme Lopes Martins <guilhermelopesmartins123@gmail.com>
2023-11-06 20:48:34 -0300

    incio projeto

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const carroController = require("./controller/carroController");
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swaggerConfig'); 
+
+//MIDDLEWARE
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
+    next();
+  })
+app.use(express.json())
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//PORT
+app.listen(8989, () => {
+    console.log("Server is running on port 8989")
+})
+
+//npx sequelize-cli model:generate --name Carro --attributes modelo:string,marca:string,ano:integer,cambio:boolean,combustivel:string,motorizacao:string,velocidade_max:integer
